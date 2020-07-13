9abe9dab56627508a8501014a9881a433ae52fb1
vineethcode33 <vineethcode@outlook.com>
2020-07-12 21:06:39 -0400

    added header componeent

---
+++ src/HeaderBar.js
@@ -0,0 +1,39 @@
+import React, { useState } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
+
+import "./HeaderBar.css";
+
+const HeaderBar = (props) => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+
+  return (
+    <div>
+      <Navbar light expand="md" className="header-bar">
+        <NavbarBrand href="/">NC</NavbarBrand>
+        <NavbarBrand href="/">News Curator</NavbarBrand>
+        <NavbarToggler onClick={toggle} />
+        <Collapse isOpen={isOpen} navbar>
+          <Nav className="mr-auto" navbar>
+            <NavItem>
+              <NavLink href="">Trending</NavLink>
+            </NavItem>
+            <NavItem>
+              <NavLink href="">Latest</NavLink>
+            </NavItem>
+          </Nav>
+        </Collapse>
+      </Navbar>
+    </div>
+  );
+};
+
+export default HeaderBar;
